92662f81a2926dc8406dd66bf5ec869ba2415fc5
Rizal Yogi Pratama <rizalyogip@gmail.com>
2017-05-17 17:48:03 +0700

    rerata auto fix

--- controller/rata_sensor.js
+++ controller/rata_sensor.js
@@ -10,6 +10,7 @@ var db = fb.database();
 const ref_kandang = db.ref('/kandangmirror/s');
 ref_kandang.on('child_changed', function(snapshot){
 	childSnapshot = snapshot.val();
+	console.log(snapshot.key);
 	console.log('First '+childSnapshot.id_kandang);
 	const ref_sensor = db.ref('/kandangmirror/s');
 	ref_sensor.once('value', function(snapshot2){
@@ -19,28 +20,29 @@ ref_kandang.on('child_changed', function(snapshot){
 		snapshot2.forEach((sensor)=>{
 				var values = sensor.val();
 				
-				if((values.id_kandang === childSnapshot.id_kandang) && ((snapshot.key).substr(0,1) !== 'W')){
+				if((values.id_kandang === childSnapshot.id_kandang) && ((snapshot.key).substr(0,1) !== 'W') && (values.id_kandang !== 'undefined')){
 					suhu = suhu + parseFloat(values.a);
 					kelembapan = kelembapan + parseFloat(values.b);
 					count++;
 				}
 		});
-		var rerataSuhu = suhu/count;
-		var rerataLembab = kelembapan/count;
+		if(((snapshot.key).substr(0,1) !== 'W') || (childSnapshot.id_kandang !== 'undefined')){
+			var rerataSuhu = suhu/count;
+			var rerataLembab = kelembapan/count;
 
 
-		var kandang = childSnapshot.id_kandang;
-		var update_ref = db.ref().child('kandangmirror/rata_sensor/kandang'+kandang);
-		var data = {
-			a : rerataSuhu,
-			b : rerataLembab
-		};
+			var kandang = childSnapshot.id_kandang;
+			var update_ref = db.ref().child('kandangmirror/rata_sensor/kandang'+kandang);
+			var data = {
+				a : rerataSuhu,
+				b : rerataLembab
+			};
 
+			
 		
-	
-    	update_ref.update(data).then(function(update){
-    		console.log('update rata sensor '+snapshot.key+' with value a '+rerataSuhu);
-    	});
-
+	    	update_ref.update(data).then(function(update){
+	    		console.log('update rata sensor '+snapshot.key+' with value a '+rerataSuhu);
+	    	});
+		}
 	})
 });
